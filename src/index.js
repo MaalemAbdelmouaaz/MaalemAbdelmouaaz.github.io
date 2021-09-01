cd745a8de281763ff3502fb29f8ee249c736ab54
MaalemAbdelmouaaz <75955950+MaalemAbdelmouaaz@users.noreply.github.com>
2021-09-01 03:53:20 +0100

    added touch option

--- src/index.js
+++ src/index.js
@@ -128,7 +128,50 @@ newButton.addEventListener("click", (event) => {
 
 const myWorker = new Worker("worker.js");
 document.addEventListener("keydown", eventsHandler);
+document.addEventListener("touchstart", handleTouchStart, false);
+document.addEventListener("touchmove", handleTouchMove, false);
 
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches;
+}
+
+function handleTouchStart(evt) {
+  const firstTouch = getTouches(evt)[0];
+  xDown = firstTouch.clientX;
+  yDown = firstTouch.clientY;
+}
+
+function handleTouchMove(evt) {
+  if (!xDown || !yDown) {
+    return;
+  }
+
+  var xUp = evt.touches[0].clientX;
+  var yUp = evt.touches[0].clientY;
+
+  var xDiff = xDown - xUp;
+  var yDiff = yDown - yUp;
+  let event = {};
+  if (Math.abs(xDiff) > Math.abs(yDiff)) {
+    if (xDiff > 0) {
+      event.key = "ArrowRight";
+    } else {
+      event.key = "ArrowLeft";
+    }
+  } else {
+    if (yDiff > 0) {
+      event.key = "ArrowDown";
+    } else {
+      event.key = "ArrowUp";
+    }
+  }
+  eventsHandler(event);
+  xDown = null;
+  yDown = null;
+}
 function eventsHandler(event) {
   if (
     !["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"].includes(event.key)
